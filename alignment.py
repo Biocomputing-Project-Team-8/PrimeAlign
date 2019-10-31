d258103fd9ad6e09e8ed63b960a3e70c6925d156
HellPwnage1337 <hellpwnage1337@gmail.com>
2019-10-31 14:26:58 -0500

    Add files via upload

---
+++ alignment.py
@@ -0,0 +1,63 @@
+import math
+
+def convert(seq, num):
+    for a in seq:
+        if a == 'A':
+            num.append(2)
+        elif a == 'C':
+            num.append(3)
+        elif a == 'G':
+            num.append(5)
+        elif a == 'T':
+            num.append(7)
+        elif a == '_':
+            num.append(1)
+        else:
+            return -1
+    return num
+
+
+def compress(num):
+    for i in range(0,len(num)-1):
+        num[i] = num[i] * num[i+1]
+    return num[0:len(num)-1]
+
+
+def search(x, target):
+    for i in range(0, len(x)):
+        if x[i] == target:
+            return i
+    return ''
+
+
+def compare(a, b):
+    diff = []
+    for i in range(0, len(a)):
+        temp = search(b, a[i])
+        if temp != '':
+            diff.append(temp - i)
+        else:
+            diff.append('x')
+
+    print(diff)
+    for j in diff:
+        if j != 'x':
+            return j
+
+
+seq1 = "AGTCCT"
+seq2 = "TAGTCGA"
+num1 = []
+num2 = []
+num1 = convert(seq1, num1)
+num2 = convert(seq2, num2)
+print(num1, num2)
+for i in range(0, math.ceil(min(len(seq1), len(seq2))/2)):
+    num1 = compress(num1)
+    num2 = compress(num2)
+    print(num1, num2)
+ans = compare(num1, num2)
+if ans > 0:
+    print("Move sequence " + str(ans) + " spaces to the right")
+elif ans < 0:
+    print("Move sequence " + abs(ans) + " spaces to the left")
